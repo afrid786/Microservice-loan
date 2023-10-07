5ffff83545440f0dc5e59b7fb9b8ce06fabe1db7
afrid786 <jobsforafrid@gmail.com>
2023-10-07 22:10:48 +0530

    Enabling spring cloud config client to fetch configs from git repo

--- src/main/java/com/afrid/loan/LoanApplication.java
+++ src/main/java/com/afrid/loan/LoanApplication.java
@@ -1,9 +1,14 @@
 package com.afrid.loan;
 
+import com.afrid.loan.dto.LoanContactInfo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(
+		value = {LoanContactInfo.class}
+)
 public class LoanApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/afrid/loan/controller/LoansController.java
+++ src/main/java/com/afrid/loan/controller/LoansController.java
@@ -1,13 +1,10 @@
 package com.afrid.loan.controller;
 
-import com.afrid.loan.dto.LoanDTO;
-import com.afrid.loan.dto.LoanRequestDTO;
-import com.afrid.loan.dto.LoanUpdateDTO;
-import com.afrid.loan.dto.ResponseDTO;
+import com.afrid.loan.dto.*;
 import com.afrid.loan.loanConstants.LoanConstants;
 import com.afrid.loan.service.ILoanService;
 import jakarta.validation.Valid;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +13,17 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api/v1",produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 public class LoansController {
 
-    ILoanService iLoanService;
+    private final ILoanService iLoanService;
+
+    @Autowired
+    LoanContactInfo loanContactInfo;
+
+    public LoansController(ILoanService iLoanService) {
+        this.iLoanService = iLoanService;
+    }
 
     @GetMapping("/loanDetails")
     public ResponseEntity<LoanDTO> getLoanDetails (@RequestParam String mobileNumber) {
@@ -65,5 +68,11 @@ public class LoansController {
             return ResponseEntity.status(HttpStatus.OK)
                     .body(new ResponseDTO(LoanConstants.STATUS_500,LoanConstants.MESSAGE_500));
         }
+    }
+
+    @GetMapping("/build-info")
+    public ResponseEntity<LoanContactInfo> buildInfo () {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(loanContactInfo);
     }
 }
