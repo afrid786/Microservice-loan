2563b8a4dd19d5ffebfb82f07b548349ea26e7d2
afrid786 <jobsforafrid@gmail.com>
2023-10-08 12:41:51 +0530

    Adding dynamic refresh logic for latest configs to populate in microservices

--- src/main/java/com/afrid/loan/dto/LoanContactInfo.java
+++ src/main/java/com/afrid/loan/dto/LoanContactInfo.java
@@ -1,5 +1,7 @@
 package com.afrid.loan.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -7,5 +9,11 @@ import java.util.Map;
 
 
 @ConfigurationProperties(prefix = "loans")
-public record LoanContactInfo(String message, Map<String,String>  contactDetails, List<String> onCallSupport) {
+@Getter
+@Setter
+public class LoanContactInfo {
+    private String message;
+    private Map<String,String>  contactDetails;
+    private List<String> onCallSupport;
+
 }
